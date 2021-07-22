d742dfab43ed6be76d221bd482f1db9cb4b8d5ad
jjg2362 <wornjs2362@gmail.com>
2021-07-22 19:38:56 +0900

    add tag key type

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,27 +1,27 @@
+export enum ETagKey {
+    KIND = 'kind',
+    FROM = 'from',
+    BRAND = 'brand',
+}
+
 export interface ITag {
-    key: string,
+    key: ETagKey,
     name: string,
 };
-
-export type TTagList = ITag[];
 
 export interface IBeer {
     id: number,
     name: string,
     image: string,
-    tags: TTagList,
+    tags: ITag[],
     price: number,
     stock: number,
 };
-
-export type TBeerList = IBeer[];
 
 export interface IPurchaseItem {
     id: number,
     count: number,
 };
-
-export type TPurchaseItem = IPurchaseItem[];
 
 export interface IPurchaseResult {
     totalCount: number,
