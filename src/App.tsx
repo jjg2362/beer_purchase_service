4fb60eca5d7408fef3f53d286982aeb10cd1f919
jjg2362 <wornjs2362@gmail.com>
2021-07-23 14:20:36 +0900

    add span component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./app.css";
+import Span from "./components/atoms/span";
 import useFetchBeers from "./hooks/useFetchBeers";
 import useFetchTags from "./hooks/useFetchTags";
 
@@ -7,7 +8,11 @@ const App: React.FC = () => {
   const { beerList } = useFetchBeers();
   const { tagList } = useFetchTags();
 
-  return <div>hello</div>;
+  return (
+    <div>
+      <Span size={16}>Hello</Span>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/atoms/span/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import classNames from "classnames";
+
+import { ColorType, ColorPalette } from "../../../models/color";
+import { CalculateBox } from "../../../utils";
+import styles from "./style.module.css";
+
+export enum TextAlign {
+  LEFT = "left",
+  CENTER = "center",
+  RIGHT = "right",
+}
+
+interface IProps {
+  children: React.ReactNode;
+  className?: string;
+  lineHeight?: number;
+  weight?: number;
+  color?: ColorType;
+  size?: number;
+  margin?: [number, number?, number?, number?];
+  align?: TextAlign;
+}
+
+const Span: React.FC<IProps> = ({
+  children,
+  className,
+  lineHeight = 1,
+  weight = 300,
+  color = ColorPalette.Black.BLACK,
+  size = 12,
+  margin = [0],
+  align = TextAlign.LEFT,
+}) => {
+  const classProps = classNames(styles.default, className);
+  const styleProps = {
+    fontSize: size,
+    color,
+    fontWeight: weight,
+    lineHeight,
+    margin: CalculateBox(margin),
+    textAlign: align,
+  };
+
+  return (
+    <span className={classProps} style={styleProps}>
+      {children}
+    </span>
+  );
+};
+
+export default Span;
